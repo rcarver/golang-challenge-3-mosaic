da943b877d024f36f7861027b49d20f599878929
Ryan Carver <ryan@ryancarver.com>
2015-05-12 18:01:13 -0700

    rewrite the CLI

--- cli/main.go
+++ cli/main.go
@@ -4,88 +4,152 @@ import (
 	"flag"
 	"fmt"
 	"image"
-	"image/color/palette"
-	"image/png"
-	"io"
+	"image/jpeg"
 	"os"
+	"path"
 
 	"github.com/rcarver/golang-challenge-3-mosaic/instagram"
 	"github.com/rcarver/golang-challenge-3-mosaic/mosaic"
 	"github.com/rcarver/golang-challenge-3-mosaic/service"
 )
 
-var tag = "balloon"
-var inventorySize = 400
-var solidPalette = false
-var runServer bool
+var (
+	command    string
+	tag        string
+	tagDirName string
+	imgDirName string
+	inName     string
+	outName    string
+	units      int
+	numImages  int
+	solid      bool
+)
+
+var help = `
+# Download images by tag
+mosaic -run download -dir images -tag balloon 
+
+# Generate mosaic with tag
+mosaic -run generate -dir images -tag balloon -in balloon.jpg -out balloon-mosaic.jpg
+`
 
 func init() {
-	flag.BoolVar(&runServer, "serve", false, "run the server")
-	flag.Parse()
+	flag.StringVar(&command, "run", "", "command to run: download | generate | serve")
+
+	// Download, Generate
+	flag.StringVar(&tag, "tag", "cat", "image tag to use")
+	flag.StringVar(&tagDirName, "dir", "./images", "dir to store images by tag")
+
+	// Download
+	flag.IntVar(&numImages, "num", 1000, "number of images to download")
+
+	// Generate
+	flag.StringVar(&inName, "in", "", "image file to read")
+	flag.StringVar(&outName, "out", "./mosaic.jpg", "image file to write")
+	flag.StringVar(&imgDirName, "imgdir", "", "dir to find images (uses tagdir + tag by default)")
+	flag.IntVar(&units, "units", 40, "number of units wide to generate the mosaic")
+	flag.BoolVar(&solid, "solid", false, "generate a mosaic with solid colors, not images")
 }
 
 func main() {
-	if runServer {
-		service.Serve()
-		return
-	}
-	units := 40
-	thumbSize := 150
-	var p *mosaic.ImagePalette
+	flag.Parse()
 
-	if solidPalette {
-		p = mosaic.NewSolidPalette(palette.WebSafe, thumbSize, thumbSize)
+	// dir is imgDirName if set, or join(tagDirName, tag)
+	var dir string
+	if imgDirName != "" {
+		dir = imgDirName
 	} else {
-		cache := mosaic.FileImageCache{"./cache"}
-		inv := mosaic.NewImageInventory(cache)
+		dir = path.Join(tagDirName, tag)
+	}
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Printf("Error initializing: %s\n", err)
+	}
 
-		api := instagram.NewClient()
-		if err := inv.Fetch(api, tag, inventorySize); err != nil {
-			fmt.Printf("Fetch() %s\n", err)
+	// inventory reads and writes from the dir.
+	inventory := newInventory(dir)
+
+	switch command {
+	case "download":
+		if err := downloadImages(tag, numImages, inventory); err != nil {
+			fmt.Printf("Download error: %s\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	case "generate":
+		if inName == "" {
+			fmt.Printf("Missing -in file\n")
 			os.Exit(1)
 		}
 
-		p = mosaic.NewImagePalette(256, thumbSize, thumbSize)
-		err := inv.PopulatePalette(tag, p)
+		// Read and decode input image.
+		in, err := os.Open(inName)
 		if err != nil {
-			fmt.Printf("PopulatePalette() %s\n", err)
+			fmt.Printf("Error initializing: %s\n", err)
 			os.Exit(1)
 		}
-	}
+		defer in.Close()
+		src, _, err := image.Decode(in)
+		if err != nil {
+			fmt.Printf("Error initializing: %s\n", err)
+			os.Exit(1)
+		}
 
-	fmt.Printf("Palette size %d with %d images\n", p.Size(), inventorySize)
+		// Generate the mosaic.
+		img, err := generateMosaic(src, tag, units, solid, inventory)
+		if err != nil {
+			fmt.Printf("Error generating: %s\n", err)
+			os.Exit(1)
+		}
 
-	fi, err := os.Open("./fixtures/balloon.jpg")
-	if err != nil {
-		fmt.Printf("os.Open() %s\n", err)
-		os.Exit(1)
-	}
-	defer fi.Close()
-	src, err := decodeImage(fi)
-	if err != nil {
-		fmt.Printf("decodeImage() %s\n", err)
-		os.Exit(1)
-	}
+		// Encode and write the output image.
+		out, err := os.Create(outName)
+		if err != nil {
+			fmt.Printf("Error outputting: %s\n", err)
+			os.Exit(1)
+		}
+		defer out.Close()
+		err = jpeg.Encode(out, img, nil)
+		if err != nil {
+			fmt.Printf("Error outputting: %s\n", err)
+			os.Exit(1)
+		}
 
-	m := mosaic.Compose(src, units, units, p)
-
-	out, err := os.Create("./output.png")
-	if err != nil {
-		fmt.Printf("os.Create() %s\n", err)
-		os.Exit(1)
-	}
-	defer out.Close()
-	err = png.Encode(out, m)
-	if err != nil {
-		fmt.Printf("png.Encode() %s\n", err)
+		os.Exit(0)
+	case "serve":
+		service.Serve()
+		os.Exit(0)
+	default:
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 }
 
-func decodeImage(reader io.Reader) (image.Image, error) {
-	img, _, err := image.Decode(reader)
-	if err != nil {
-		return nil, err
+func newInventory(dir string) *mosaic.ImageInventory {
+	cache := mosaic.FileImageCache{dir}
+	return mosaic.NewImageInventory(cache)
+}
+
+func downloadImages(tag string, numImages int, inv *mosaic.ImageInventory) error {
+	api := instagram.NewClient()
+	if err := inv.Fetch(api, tag, numImages); err != nil {
+		return err
 	}
-	return img, err
+	return nil
+}
+
+var (
+	// Number of colors in the mosaic color palette.
+	paletteSize = 256
+	// Size of the mosaic thumbnail images.
+	thumbSize = 150
+)
+
+func generateMosaic(src image.Image, tag string, units int, solid bool, inv *mosaic.ImageInventory) (image.Image, error) {
+	p := mosaic.NewImagePalette(paletteSize, thumbSize, thumbSize)
+	if !solid {
+		if err := inv.PopulatePalette(p); err != nil {
+			return nil, err
+		}
+	}
+	return mosaic.Compose(src, units, units, p), nil
 }
--- mosaic/inventory.go
+++ mosaic/inventory.go
@@ -8,22 +8,121 @@ import (
 	"image/jpeg"
 	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/rcarver/golang-challenge-3-mosaic/instagram"
 )
 
-type ImageCache interface {
-	Put(key string, m image.Image) error
-	Get(key string) (image.Image, error)
-	Has(key string) bool
+// ImageInventory fetches and uses images to drive mosaic creation.
+type ImageInventory struct {
+	cache ImageCache
 }
 
+// NewImageInventory creates an inventory using the given cache.
+func NewImageInventory(cache ImageCache) *ImageInventory {
+	return &ImageInventory{
+		cache: cache,
+	}
+}
+
+// PopulatePalette pulls images from the inventory and adds them to a palette.
+func (ii *ImageInventory) PopulatePalette(palette *ImagePalette) error {
+	keys, err := ii.cache.Keys()
+	if err != nil {
+		return err
+	}
+	for _, key := range keys {
+		m, err := ii.cache.Get(key)
+		if err != nil {
+			//log.Printf("Error reading from cache: %s, key:%#v\n", err, key)
+			continue
+		}
+		palette.Add(m)
+	}
+	return nil
+}
+
+// Fetch pulls new images from the api and adds them to the inventory.
+func (ii *ImageInventory) Fetch(api *instagram.Client, tag string, max int) error {
+	res, err := api.Tagged(tag, "")
+	if err != nil {
+		return err
+	}
+	for {
+		for _, m := range res.Media {
+			if ii.cache.Size() >= max {
+				return nil
+			}
+			err := ii.cacheImage(m)
+			if err != nil {
+				return err
+			}
+		}
+		log.Printf("Fetch(%s) got %d of %d\n", tag, ii.cache.Size(), max)
+		res, err = api.Tagged(tag, res.MaxTagID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Size returns the number of images in the inventory.
+func (ii *ImageInventory) Size(tag string) int {
+	return ii.cache.Size()
+}
+
+func (ii *ImageInventory) cacheImage(media instagram.Media) error {
+	res := media.ThumbnailImage()
+	key := ii.cache.Key(res.URL)
+	if ii.cache.Has(key) {
+		//log.Printf("Has %s\n", res.URL)
+		return nil
+	}
+	img, err := res.Image()
+	if err != nil {
+		return nil
+	}
+	//log.Printf("Get %s\n", res.URL)
+	if err := ii.cache.Put(key, img); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ImageCacheKey identifies an image in the cache.
+type ImageCacheKey string
+
+// ImageCache is the interface for any cache implementation.
+type ImageCache interface {
+	// Key returns a consistent cache key from string.
+	Key(name string) ImageCacheKey
+	// Put stores an image in the cache by key.
+	Put(ImageCacheKey, image.Image) error
+	// Get returns an image in the cache by key
+	Get(ImageCacheKey) (image.Image, error)
+	// Has returns true if an image exists at key.
+	Has(ImageCacheKey) bool
+	// Keys returns a list of the stored keys, unordered.
+	Keys() ([]ImageCacheKey, error)
+	// Size returns the number of images stores.
+	Size() int
+}
+
+// FileImageCache implements an ImageCache on the filesystem.
 type FileImageCache struct {
 	Dir string
 }
 
-func (c FileImageCache) Put(key string, m image.Image) error {
-	fo, err := os.Create(c.path(key))
+func (c FileImageCache) Key(name string) string {
+	k := sha1.Sum([]byte(name))
+	return hex.EncodeToString(k[:])
+}
+
+func (c FileImageCache) Put(key ImageCacheKey, m image.Image) error {
+	fo, err := os.Create(c.keyToPath(key))
 	if err != nil {
 		return err
 	}
@@ -34,8 +133,11 @@ func (c FileImageCache) Put(key string, m image.Image) error {
 	return nil
 }
 
-func (c FileImageCache) Get(key string) (image.Image, error) {
-	fi, err := os.Open(c.path(key))
+func (c FileImageCache) Get(key ImageCacheKey) (image.Image, error) {
+	fi, err := os.Open(c.keyToPath(key))
+	if err != nil {
+		return nil, err
+	}
 	defer fi.Close()
 	m, err := jpeg.Decode(fi)
 	if err != nil {
@@ -44,86 +146,37 @@ func (c FileImageCache) Get(key string) (image.Image, error) {
 	return m, nil
 }
 
-func (c FileImageCache) Has(key string) bool {
-	if _, err := os.Stat(c.path(key)); os.IsNotExist(err) {
+func (c FileImageCache) Keys() ([]ImageCacheKey, error) {
+	list, err := filepath.Glob(path.Join(c.Dir, "*.jpg"))
+	if err != nil {
+		return []ImageCacheKey{}, err
+	}
+	keys := make([]ImageCacheKey, len(list))
+	for i, o := range list {
+		keys[i] = c.pathToKey(o)
+	}
+	return keys, nil
+}
+
+func (c FileImageCache) Size() int {
+	list, err := c.Keys()
+	if err == nil {
+		return len(list)
+	}
+	return 0
+}
+
+func (c FileImageCache) Has(key ImageCacheKey) bool {
+	if _, err := os.Stat(c.keyToPath(key)); os.IsNotExist(err) {
 		return false
 	}
 	return true
 }
 
-func (c FileImageCache) path(key string) string {
-	k := sha1.Sum([]byte(key))
-	h := hex.EncodeToString(k[:])
-	return fmt.Sprintf("%s/%s.jpg", c.Dir, h)
+func (c FileImageCache) keyToPath(key ImageCacheKey) string {
+	return fmt.Sprintf("%s/%s.jpg", c.Dir, key)
 }
 
-type ImageInventory struct {
-	ImageCache
-	Tags map[string][]string
-}
-
-func NewImageInventory(cache ImageCache) *ImageInventory {
-	return &ImageInventory{
-		ImageCache: cache,
-		Tags:       make(map[string][]string),
-	}
-}
-
-func (i ImageInventory) PopulatePalette(tag string, palette *ImagePalette) error {
-	keys := i.Tags[tag]
-	for _, key := range keys {
-		m, err := i.Get(key)
-		if err != nil {
-			log.Printf("Error reading from cache: %s, key:%#v\n", err, key)
-			continue
-		}
-		palette.Add(m)
-	}
-	return nil
-}
-
-func (i ImageInventory) Size(tag string) int {
-	return len(i.Tags[tag])
-}
-
-func (ii *ImageInventory) Fetch(api *instagram.Client, tag string, max int) error {
-	res, err := api.Tagged(tag, "")
-	if err != nil {
-		return err
-	}
-	for {
-		for _, m := range res.Media {
-			if len(ii.Tags[tag]) >= max {
-				return nil
-			}
-			key, err := ii.cacheImage(m)
-			if err != nil {
-				return err
-			}
-			ii.Tags[tag] = append(ii.Tags[tag], key)
-		}
-		log.Printf("Fetch(%s) got %d of %d\n", tag, len(ii.Tags[tag]), max)
-		res, err = api.Tagged(tag, res.MaxTagID)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (ii *ImageInventory) cacheImage(media instagram.Media) (string, error) {
-	res := media.ThumbnailImage()
-	if ii.Has(res.URL) {
-		//log.Printf("Has %s\n", res.URL)
-		return res.URL, nil
-	}
-	img, err := res.Image()
-	if err != nil {
-		return "", nil
-	}
-	//log.Printf("Get %s\n", res.URL)
-	if err := ii.Put(res.URL, img); err != nil {
-		return "", err
-	}
-	return res.URL, nil
+func (c FileImageCache) pathToKey(path string) ImageCacheKey {
+	return ImageCacheKey(strings.Trim(filepath.Base(path), filepath.Ext(path)))
 }
